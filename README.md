129cc4b092f31491e0a1767ca3407b6f2f43cd73
Alexandre Barbosa Quiterio <49036660+alequiterio@users.noreply.github.com>
2019-11-10 13:23:49 -0300

    Fullstack Lab

--- README.md
+++ README.md
@@ -1,29 +1,28 @@
-# Docz Example
+<img src="./bugtracker.png">
 
-This directory is a brief example of a [Docz](https://www.docz.site/) site that can be deployed with ZEIT Now and zero configuration.
+Projeto resultado do FullStack Lab Da [DevPleno](https://www.devpleno.com/)
 
-## Deploy Your Own
+# Sobre
 
-Deploy your own Docz project with ZEIT Now.
+Feito em Node, O projeto BugTracker tem como objetivo coletar de maneira simples e rápida, relatos de bugs em sistemas. Se encaixa perfeitamente em qualquer empresa que trabalhe com desenvolvimento de sistemas.
 
-[![Deploy with ZEIT Now](https://zeit.co/button)](https://zeit.co/new/project?template=https://github.com/zeit/now-examples/tree/master/docz)
+Os dados coletados no formulário, vão para uma planilha do google e graças a praticidade das ferramentas do google, a planilha pode ser facilmente compartilhada com pessoas que precisem ter acesso a essas informaçõe e solucionar o problema.
 
-*Live Example: https://docz.now-examples.now.sh*
+# Foram usados
 
-### How We Created This Example
+* ejs
+* google-spreadsheet API
+* express
+* sendgrid
 
-To get started with Docz for deployment with ZEIT Now, you can use the [Now CLI](https://zeit.co/download) to initialize the project:
 
-```shell
-$ now init docz
-```
+#preview
 
-> The only change made is to add `dest: '/public'` to the `doczrc.js` file.
+<img src="./form.png">
+Formulário de arrecadação de informações.
+<br>
+<br>
+<br>
 
-### Deploying From Your Terminal
-
-You can deploy your new Docz project with a single command from your terminal using [Now CLI](https://zeit.co/download):
-
-```shell
-$ now
-```
+<img src="./success.png">
+Aviso de que o relato foi enviado com sucesso.
---
+++ index.js
@@ -0,0 +1,99 @@
+const express = require('express')
+const path = require('path') // core
+const app = express()
+const { promisify } = require('util')
+const sgMail = require('@sendgrid/mail')
+
+const GoogleSpreadSheet = require('google-spreadsheet')
+const credentials = require ('./bugtracker.json')
+
+app.use (express.json())
+app.use (express.urlencoded({ extended: true }))
+
+app.set('view engine', 'ejs')
+app.set ('views', path.resolve (__dirname, 'views'))
+
+// configurações
+
+const sgKey = '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'
+const documentID = '1iNiSZewugNysXnT6ttiC5WONBnDWDxHvBKvXNODm2EQ'
+const wsheetIndex = 0
+
+app.get('/', (req, res) => {
+
+    res.render ('home')
+})
+
+app.post('/', async (req, res) => {
+
+    try
+    {
+        const {
+            name,
+            email,
+            issuetype,
+            howtoreproduce,
+            expectedoutput,
+            receivedoutput,
+            userAgent,
+            userDate
+
+        } = req.body
+
+        const doc = new GoogleSpreadSheet (documentID)
+
+        await promisify (doc.useServiceAccountAuth)(credentials)
+
+        const info = await promisify (doc.getInfo)()
+
+        const worksheet = info.worksheets [wsheetIndex]
+
+        await promisify (worksheet.addRow)({
+
+            name,
+            email,
+            issuetype,
+            source: req.query.source || "default",
+            howtoreproduce,
+            expectedoutput,
+            receivedoutput,
+            userAgent,
+            userDate
+        })
+
+        // se for crítico
+
+        if (issuetype === 'critical')
+        {
+            sgMail.setApiKey(sgKey)
+            const msg = {
+                to: 'goo.alexandrebarbosaquiterio@gmail.com',
+                from: 'goo.alexandrebarbosaquiterio@gmail.com',
+                subject: 'Bug critico reportado',
+                text: `O usuário ${req.body.name} reportou um erro crítico.`,
+                html: `O usuário ${req.body.name} reportou um erro crítico.`,
+            }
+            await sgMail.send(msg)
+        }
+
+        res.render ('success')
+    }
+    catch (error)
+    {
+        res.send ("erro ao relatar bug")
+
+        console.log (error)
+    }
+})
+
+app.listen(3000, err => {
+
+    if (err)
+    {
+        console.log ('erro ao subir servidor: ', err)
+    }
+    else
+    {
+        console.log ('BugTracker rodando...')
+    }
+})
